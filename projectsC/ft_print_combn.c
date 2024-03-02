1aa2df5a1e2ca890386a9e66a5cb1f20d981e1f5
joCol-git <joao.r.colaco@gmail.com>
2024-03-02 02:37:27 +0000

    declare functions in top

--- projectsC/ft_print_combn.c
+++ projectsC/ft_print_combn.c
@@ -1,33 +1,18 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void print_combinations(int data[], int n, int *first) {
-    char buffer[20];
-    int buffer_index = 0;
+void ft_print_combn(int n);
+void generate_combinations(int data[], int start, int end, int index, int n, int *first);
+void print_combinations(int data[], int n, int *first);
 
-    if (!(*first)) {
-        char comma = ',';
-        write(1, &comma, 1);
-    } else {
-        *first = 0;
-    }
-    for (int i = 0; i < n; i++) {
-        buffer[buffer_index++] = data[i] + '0';
-    }
-    write(1, buffer, buffer_index);
+int main() {
+    int n;
+    printf("Digite o valor de n (0 < n < 10): ");
+    scanf("%d", &n);
 
-}
+    ft_print_combn(n);
 
-void generate_combinations(int data[], int start, int end, int index, int n, int *first) {
-    if (index == n) {
-        print_combinations(data, n, first);
-        return;
-    }
-
-    for (int i = start; i <= end; i++) {
-        data[index] = i;
-        generate_combinations(data, i + 1, end, index + 1, n, first);
-    }
+    return 0;
 }
 
 void ft_print_combn(int n) {
@@ -46,12 +31,33 @@ void ft_print_combn(int n) {
     generate_combinations(data, floor, roof, floor, n, &first);
 }
 
-int main() {
-    int n;
-    printf("Digite o valor de n (0 < n < 10): ");
-    scanf("%d", &n);
+void generate_combinations(int data[], int start, int end, int index, int n, int *first) {
+    if (index == n) {
+        print_combinations(data, n, first);
+        return;
+    }
 
-    ft_print_combn(n);
-
-    return 0;
+    for (int i = start; i <= end; i++) {
+        data[index] = i;
+        generate_combinations(data, i + 1, end, index + 1, n, first);
+    }
 }
+
+
+void print_combinations(int data[], int n, int *first) {
+    char buffer[20];
+    int buffer_index = 0;
+
+    if (!(*first)) {
+        char comma = ',';
+        write(1, &comma, 1);
+    } else {
+        *first = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        buffer[buffer_index++] = data[i] + '0';
+    }
+    write(1, buffer, buffer_index);
+
+}
+
