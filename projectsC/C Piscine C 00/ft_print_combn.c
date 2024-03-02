e2ad6e077b54383dc461e9bff1a53636e17b1cda
joCol-git <joao.r.colaco@gmail.com>
2024-03-02 03:57:29 +0000

    add variables for customization

--- projectsC/C Piscine C 00/ft_print_combn.c
+++ projectsC/C Piscine C 00/ft_print_combn.c
@@ -1,21 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void ft_print_combn(int *counter, int n, int roof, int floor);
+void ft_print_combn(int *counter, int n, int digit_floor, int digit_roof, int n_floor, int n_roof);
 void generate_combinations(int *counter, int data[], int start, int end, int index, int n, int *first);
 void print_combinations(int data[], int n, int *first);
 void ft_putnbr(int n);
 
 int main() {
-    int floor = 0;
-    int roof = 9;
+    int n_floor = 1,
+        n_roof = 9,
+        digit_floor = 0,
+        digit_roof = 9;
+
     int counter = 0;
     int n;
 
-    printf("Digite o valor de n (%d < n < %d): ", floor, roof);
+    printf("Digite o valor de n (%d <= n <= %d): ", n_floor, n_roof);
     scanf("%d", &n);
 
-    ft_print_combn(&counter, n, floor, roof);
+    ft_print_combn(&counter, n, digit_floor, digit_roof, n_floor, n_roof);
 
     printf("\nTotal: ");
     ft_putnbr(counter);
@@ -24,8 +27,8 @@ int main() {
     return 0;
 }
 
-void ft_print_combn(int *counter, int n, int floor, int roof) {
-    if (n <= floor && n >= roof) {
+void ft_print_combn(int *counter, int n, int digit_floor, int digit_roof, int n_floor, int n_roof) {
+    if (n < n_floor && n > n_roof) {
         char error_message[] = "Valor de n fora do intervalo permitido.\n";
         write(1, error_message, sizeof(error_message) - 1);
         return;
@@ -34,7 +37,7 @@ void ft_print_combn(int *counter, int n, int floor, int roof) {
     int data[n];
     int first = 1;
 
-    generate_combinations(counter, data, floor, roof, floor, n, &first);
+    generate_combinations(counter, data, digit_floor, digit_roof, digit_floor, n, &first);
 }
 
 void generate_combinations(int *counter, int data[], int start, int end, int index, int n, int *first) {
