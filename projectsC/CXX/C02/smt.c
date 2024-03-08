278cce8eea1f1d35da71b5f53ce947720811bd05
jColaco <me@jColaco.dev>
2024-03-08 14:47:53 +0000

    capitalize

--- projectsC/CXX/C02/smt.c
+++ projectsC/CXX/C02/smt.c
@@ -10,9 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
-{
-  
- int i = 0;
-   return 0;
+#include <stdio.h>
+
+int isAlphanumeric(char c) {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
+}
+
+void capitalizeWords(char *str) {
+    int i = 0;
+    int capitalizeNext = 1;
+    while (str[i]) {
+        if (isAlphanumeric(str[i])) {
+            if (capitalizeNext) {
+                if (str[i] >= 'a' && str[i] <= 'z') {
+                    str[i] -= 32;
+                }
+                capitalizeNext = 0;
+            } else {
+                if (str[i] >= 'A' && str[i] <= 'Z') {
+                    str[i] += 32;
+                }
+            }
+        } else {
+            capitalizeNext = 1;
+        }
+        i++;
+    }
+}
+
+int main() {
+    char str[] = "hello world! this is a test 123.my-goD-1aS";
+    capitalizeWords(str);
+    printf("%s\n", str);
+    return 0;
 }
